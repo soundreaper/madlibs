53e6474be84ef070fb26164d070097488e19684b
soundreaper@gmail.com <soundreaper@gmail.com>
2019-08-30 13:53:26 -0700

    Add what Mad Lib looks like as comment, Start function to assign color to different parts of speed

---
+++ madlibs.py
@@ -0,0 +1,52 @@
+import sys
+from termcolor import colored, cprint
+
+'''
+Pizza:
+
+Pizza was inveted by a [adjective] [nationality] chef named [person]. To make pizza, you need to take a lump
+of [noun], and make a thin, round [adjective] [noun]. Then you cover it with [adjective] sauce, [adjective] cheese,
+and fresh chopped [plural noun]. Next you have to bake it in a very hot [noun]. When it is done, cut it
+into [number] [shapes]. Some kids like [food] pizza the best, but my favorite is the [food] pizza. If I could, I would
+eat pizza [number] times a day!
+'''
+
+inputArray = ['adjective', 'nationality', 'person', 'noun', 'adjective', 'noun', 'adjective', 'adjective', 'plural noun'
+'noun', 'number', 'shapes', 'food', 'food', 'number']
+
+# DIFFERENT PARTS OF SPEECH + COLOR ASSIGN
+def partsOfSpeech(input):
+    for items in inputArray:
+        if items == "adjective":
+            colorText = colored(input, 'red')
+            return colorText
+        if items == "nationality":
+            colorText = colored(input, 'green')
+            return colorText
+        if items == "adjective":
+            colorText = colored(input, 'yellow')
+            return colorText
+
+    
+# VALID TEXT CHECK
+def textVerification(input):
+    if input.isalpha():
+        return True
+    else:
+        return False
+
+# TEST
+def test():
+    running = 1
+    while running < 16:
+        textInput = input("Enter a word: ")
+        check = textVerification(textInput)
+        if check == True:
+            print(partsOfSpeech(textInput))
+        else:
+            print("That input was invalid.")
+        running += 1
+
+test()
+
+
